1d457646d7b9404cfb55e981bb646d35b1b01fc8
Salih Zorbozan <salihzorbozan133@gmail.com>
2023-04-06 20:22:20 +0300

    v1.0.0

--- İdareDeskstop/Form1.Designer.cs
+++ İdareDeskstop/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_sifre = new System.Windows.Forms.TextBox();
+            this.btn_giris = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Şifre";
+            // 
+            // txt_sifre
+            // 
+            this.txt_sifre.Location = new System.Drawing.Point(199, 62);
+            this.txt_sifre.Name = "txt_sifre";
+            this.txt_sifre.Size = new System.Drawing.Size(146, 20);
+            this.txt_sifre.TabIndex = 1;
+            // 
+            // btn_giris
+            // 
+            this.btn_giris.Location = new System.Drawing.Point(132, 114);
+            this.btn_giris.Name = "btn_giris";
+            this.btn_giris.Size = new System.Drawing.Size(166, 26);
+            this.btn_giris.TabIndex = 2;
+            this.btn_giris.Text = "Giriş";
+            this.btn_giris.UseVisualStyleBackColor = true;
+            this.btn_giris.Click += new System.EventHandler(this.btn_giris_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(472, 168);
+            this.Controls.Add(this.btn_giris);
+            this.Controls.Add(this.txt_sifre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_sifre;
+        private System.Windows.Forms.Button btn_giris;
     }
 }
 
--- İdareDeskstop/Form1.cs
+++ İdareDeskstop/Form1.cs
@@ -7,14 +7,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
 
 namespace İdareDeskstop
 {
     public partial class Form1 : Form
     {
+
+        IFirebaseConfig ifc = new FirebaseConfig()
+        {
+            AuthSecret = "qlDUgLSDUYM1OqcOnlecbAEDhbFWJI8MCMUtZpYU",
+            BasePath = "https://kkfldatabase-default-rtdb.europe-west1.firebasedatabase.app/"
+        };
+
+        IFirebaseClient client;
+
+        Form1 form1 = new Form1();
+        ÇağırmaEkranı çağırmaEkranı= new ÇağırmaEkranı();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        List<İdareciler> idareciler = new List<İdareciler>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new FirebaseClient(ifc);
+            }
+            catch
+            {
+                MessageBox.Show("There was an error");
+            }
+
+
+
+
+            for(int i=0;i<5;i++)
+            {
+                FirebaseResponse res= client.Get(@"AdministrationList" + ("İdareci" + i));
+
+                İdareciler idr = res.ResultAs<İdareciler>();
+
+                idareciler.Add(idr);
+            }
+
+
+
+        }
+
+        private void btn_giris_Click(object sender, EventArgs e)
+        {
+            bool kontrol = false;
+
+            foreach(İdareciler idareci in idareciler)
+            {
+                if (txt_sifre.Text ==$"{idareci.idaresirano}{idareci.idareisim}")
+                {
+                    form1.Hide();
+                    çağırmaEkranı.Show();
+                    kontrol = true;
+                    break;
+                }
+            }
+            if(kontrol=false)
+            {
+                MessageBox.Show("Giriş Bilginiz Hatalıdır");
+            }
+
+           
         }
     }
 }
